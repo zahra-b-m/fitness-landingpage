f3b6d66d609bec0dc5dde71d8d510a8c48569278
zahra-b-m <zbourbour@gmail.com>
2020-11-18 10:39:37 +0330

    first commit

---
+++ js/menu.js
@@ -0,0 +1,62 @@
+
+   
+   // var menu=document.getElementById('menuBtn');
+   //  var closemenu=document.getElementById('closebtn');
+   //  var topul=document.getElementById('topUl');
+   //  menu.onclick= function(){
+   //  topul.classList.add("open")
+   //  //menuRight.className += " open";
+   //  };
+    
+   //  closemenu.onclick= function(){
+   //  topul.classList.remove("open")
+   //  };
+//     function myFunction() {
+//       var x = document.getElementById("topUl");
+//       if (x.className === "topmenu") {
+//         x.className += " open";
+//       } else {
+//         x.className = "topmenu";
+//       }
+//     }
+
+
+//     var mybutton = document.getElementById("myBtn");
+		
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+
+
+// function topFunction() {
+//   window.scrollTo({ top: 0, behavior: 'smooth' });
+ 
+// }
+
+// const a=prompt("ur age?");
+// alert(a);
+// if (confirm('are u sure?')){
+//   console.log('yes');}
+//   else{console.log('no');}
+// let val;
+// // val=window.scrollX;
+// val=window.location.port;
+// val=window.history;
+// val=window.history.go(-2);
+// val=window.navigator.vendor;
+//  console.log(val);
+// window.location="https://google.com";
+// window.location.reload();
+/*for(var i=0; i<30; i++){
+   setTimeout(function(){
+      document.write(i);
+   },i*1000);
+}
